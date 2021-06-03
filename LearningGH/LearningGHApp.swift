e7e0c2ee8803b4a8562a755dc55d6de5b5e35d29
Donald Lau <donald.lau@qualicom.com.hk>
2021-06-03 14:18:25 +0800

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Donald Lau on 3/6/2021.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
